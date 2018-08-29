b060d2cdc4b7fa1f29aa238755ec523b749f2772
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-29 14:05:55 -0700

    try to make a ready user

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,8 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:org:open
-sleep 0.1 && sfdx force:user:create -f config/userDef/cloudy-user-def.json && sfdx shane:user:photo -f assets/cloudy-profile.png -l Cloudy &
-sleep 0.1 && sfdx force:user:create -f config/userDef/codey-user-def.json && sfdx shane:user:photo -f assets/codey-profile.png  -l CodeBear &
-sleep 0.1 && sfdx force:user:create -f config/userDef/astro-user-def.json && sfdx shane:user:photo -f assets/astro-profile.png  -l Nomical &
+
+sleep 0.1 && sfdx force:user:create -f config/userDef/cloudy-user-def.json && sfdx shane:user:photo -f assets/cloudy-profile.png -l Cloudy
+sleep 0.1 && sfdx force:user:create -f config/userDef/codey-user-def.json && sfdx shane:user:photo -f assets/codey-profile.png  -l CodeBear
+sleep 0.1 && sfdx force:user:create -f config/userDef/astro-user-def.json && sfdx shane:user:photo -f assets/astro-profile.png  -l Nomical
+sfdx shane:data:file:upload -f assets/LightningExperienceReadinessReport_V5.4_08-29-2018_20-53-52
